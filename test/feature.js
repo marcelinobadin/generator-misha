b24f659fe6b09e1f795eb308e813a05aa8e13a8e
Marcelino Badin <marcelinobadin@indra.com.br>
2016-01-22 15:05:32 -0200

    Added tests for features created to myModule.subModule modules

--- test/feature.js
+++ test/feature.js
@@ -6,24 +6,49 @@
   var helpers = require('yeoman-test');
 
   describe('misha:feature', function () {
-    before(function (done) {
-      helpers.run(path.join(__dirname, '../generators/feature'))
-        .withGenerators([ // configure path to subgenerators
-          path.join(__dirname, '../generators/controller'),
-          path.join(__dirname, '../generators/template')
-        ])
-        .withArguments('name')
-        .on('end', done);
+
+    describe('newFeature with default module', function () {
+      before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/feature'))
+          .withGenerators([ // configure path to subgenerators
+            path.join(__dirname, '../generators/controller'),
+            path.join(__dirname, '../generators/template')
+          ])
+          .withArguments('newFeature')
+          .on('end', done);
+      });
+
+      it('creates files', function () {
+        assert.file([
+          'app/base/new-feature/new-feature.controller.js',
+          'test/karma/base/new-feature/new-feature.controller.spec.js',
+          'app/base/new-feature/new-feature.html',
+          'app/base/new-feature/assets',
+          'app/base/new-feature/styles'
+        ]);
+      });
     });
 
-    it('creates files', function () {
-      assert.file([
-        'app/base/name/name.controller.js',
-        'test/karma/base/name/name.controller.spec.js',
-        'app/base/name/name.html',
-        'app/base/name/assets',
-        'app/base/name/styles'
-      ]);
+    describe('newFeature with myModule.subModule module', function () {
+      before(function (done) {
+        helpers.run(path.join(__dirname, '../generators/feature'))
+          .withGenerators([ // configure path to subgenerators
+            path.join(__dirname, '../generators/controller'),
+            path.join(__dirname, '../generators/template')
+          ])
+          .withArguments(['newFeature', 'myModule.subModule'])
+          .on('end', done);
+      });
+
+      it('creates files', function () {
+        assert.file([
+          'app/my-module/sub-module/new-feature/new-feature.controller.js',
+          'test/karma/my-module/sub-module/new-feature/new-feature.controller.spec.js',
+          'app/my-module/sub-module/new-feature/new-feature.html',
+          'app/my-module/sub-module/new-feature/assets',
+          'app/my-module/sub-module/new-feature/styles'
+        ]);
+      });
     });
   });
 })();
