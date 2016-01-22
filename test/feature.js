347d8c384062c3f29455e9b1fb428120c8646d66
Marcelino Badin <marcelinobadin@indra.com.br>
2016-01-22 14:39:13 -0200

    Create unit test to feature sub generator

--- test/feature.js
+++ test/feature.js
@@ -5,12 +5,12 @@
   var assert = require('yeoman-assert');
   var helpers = require('yeoman-test');
 
-  describe('misha:route', function () {
+  describe('misha:feature', function () {
     before(function (done) {
-      helpers.run(path.join(__dirname, '../generators/route'))
+      helpers.run(path.join(__dirname, '../generators/feature'))
         .withGenerators([ // configure path to subgenerators
           path.join(__dirname, '../generators/controller'),
-          path.join(__dirname, '../generators/template'),
+          path.join(__dirname, '../generators/template')
         ])
         .withArguments('name')
         .on('end', done);
@@ -18,9 +18,9 @@
 
     it('creates files', function () {
       assert.file([
-        'app/base/main/name.controller.js',
-        'test/karma/base/main/name.controller.spec.js',
-        'app/base/main/name.html'
+        'app/base/name/name.controller.js',
+        'test/karma/base/name/name.controller.spec.js',
+        'app/base/name/name.html'
       ]);
     });
   });
